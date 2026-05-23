46e77e183d983ac4dbaeb41090248ca1325c7f88
Denis <djdenisdias@gmail.com>
2026-05-23 16:42:42 -0300

    feat: adicionado o gerenciamento de sessão e redirecionamento durante a autenticação

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,33 @@
-const DashboardPage = () => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
   return (
-    <h1 className="flex h-screen w-screen items-center justify-center">
-      DASHBOARD!
-    </h1>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+      <h1>DASHBOARD!</h1>
+      <Card className="w-[250px]">
+        <CardContent>
+          <p>Nome: {session?.user.name}</p>
+          <p>E-mail: {session?.user.email}</p>
+        </CardContent>
+        <CardFooter>
+          <SignOutButton className="w-full" />
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
